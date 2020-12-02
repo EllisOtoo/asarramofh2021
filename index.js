2a2c4239373e6e5758fac6664c955b542c792b4b
Ellis Otoo <ellisotoo@gmail.com>
2020-12-02 18:26:04 +0000

    Node mailer files added

--- index.js
+++ index.js
@@ -30,3 +30,151 @@ app.listen(3000, function () { // listen method with callback
 
 // Set static resources
 app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+app.post('/requestForProposal',  (req, res)=> {
+    const output = `
+    <p>You have a new proposal request</p>
+    <ul>
+        <li>Client Name: ${req.body.Username}</li>
+        <li>Mobile Number: ${req.body.mobileNumber}</li>
+        <li>Email Address: ${req.body.email}</li>
+        <li>Business Name and Location: ${req.body.businessNameAndLocation}</li>
+        <li>No. of License: ${req.body.licenseNumber}</li>
+        <li>Additional Content: ${req.body.AdditionalFormInfo}</li>
+        <li>Time of Request: ${new Date()}</li>
+    </ul>
+    `
+
+    var transporter = nodemailer.createTransport({
+        host: 'mail.asarramofh.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'prospects@asarramofh.com',
+            pass: 'asarfocus2020Sucess'
+        } /*,
+        tls: {
+            rejectUnauthorized: false
+        } */
+    });
+
+
+    var mailOptions = {
+        from: 'prospects@asarramofh.com',
+        to: 'ellisotoo@gmail.com, brandexcelnow@gmail.com',
+        subject: 'New AsarRamofh Prospect',
+        text: `${output}`,
+        html: output
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    res.render('quickbooksCampaignSuccess.ejs',
+    {
+        successMsg: 'Mail has been Sent!',
+        foo: "bar"
+    } );
+
+    // res.send('Thank you')
+}) 
+
+
+function handleMailRequest(req, res) {
+    console.log(req.body)
+    const output = `
+<p>You have a new newsletter request</p>
+<ul>
+    <li>${req.body.name}</li>
+    <li>${req.body.email}</li>
+    <li>${new Date()}</li>
+</ul>
+`
+
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'ellisotoo@gmail.com',
+            pass: 'Thinkglobal1a2'
+        }
+    });
+
+
+    var mailOptions = {
+        from: 'ellis@asarramofh.com',
+        to: 'brandexcelnow@gmail.com',
+        subject: 'New Asar Ramofh Contact',
+        text: `${output}`,
+        html: output
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    
+    res.render('index.ejs');
+    // console.log(output)
+}
+
+app.post('/contact', function (req, res) {
+    console.log(req.body)
+    const output = `
+<p>You have a new contact request</p>
+<ul>
+    <li>${req.body.name}</li>
+    <li>${req.body.email}</li>
+    <li>${req.body.subject}</li>
+    <li>${req.body.message}</li>
+    <li>${new Date()}</li>
+</ul>   
+`
+
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'ellisotoo@gmail.com',
+            pass: 'thinkglobal11a'
+        }
+        /*    host: 'server213.web-hosting.com',
+           port: 465,
+           secure: true,
+           auth: {
+               user: 'ellis@asarramofh.com',
+               pass: 'thinkglobal'
+           } */
+    });
+
+
+    var mailOptions = {
+        from: 'ellis@asarramofh.com',
+        to: 'brandexcelnow@gmail.com',
+        subject: 'New Asar Ramofh Contact',
+        text: `${output}`,
+        html: output
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    res.render('contactUs.ejs');
+    // console.log(output)
+});
+
+
+
+app.post('/send', handleMailRequest)
